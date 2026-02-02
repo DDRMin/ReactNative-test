a5348226e753a7a65d3897311f4ff3d5a3d8c596
DDRMin <deepthidamruvan@gmail.com>
2026-02-02 15:16:50 +0530

    fix: correct project name and slug in app.json; refactor layout to use SafeAreaProvider; add onboarding screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,18 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className=" text-dark-100 font-bold text-5xl">
-        Welcome!
-      </Text>
-    </View>
+    <SafeAreaView className="flex-1">
+      <View className="flex-1 items-center justify-center bg-light-100">
+        <Text className=" text-dark-100 font-bold text-5xl">
+          Welcome!
+        </Text>
+        <Link href="/onboarding" className="mt-4 px-4 py-2 bg-primary-500 rounded">
+          Onboarding
+        </Link>
+      </View>
+    </SafeAreaView>
   );
 }
